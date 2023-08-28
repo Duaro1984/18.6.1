7413b5a9b1fe84e30de05de554172b28f92a3c7e
Duaro1984 <elan84@inbox.ru>
2023-08-29 01:20:42 +0300

    new project added

---
+++ pythonProject10BotFor Telegram/config.py
@@ -0,0 +1,13 @@
+TOKEN = '6579489554:AAGnVswNm_1hZ7TTkn5RVAG0mdvyZtDPWVI'
+keys = {
+    'биткоин': 'BTC',
+    'эффириум': 'ETH',
+    'доллар': 'USD',
+    'Tether': 'USDT',
+    'Solana': 'SOL',
+    'Polygon': 'MATIC',
+}
+
+
+
+
